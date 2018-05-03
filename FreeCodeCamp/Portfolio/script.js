34543addebd4ff14fba56812bb8816ccb2e91381
Scylidose <jordisaleilles@hotmail.fr>
2018-05-03 14:16:37 -0400

    Script: Added animated scroll.

---
+++ FreeCodeCamp/Portfolio/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(".nav-item a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    setInterval(function () {
+        $('.nav-item a').toggleClass("bounce");
+    }, 5000);
+});
