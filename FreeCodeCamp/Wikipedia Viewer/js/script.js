fd25a8c5ce87b8f5dd7cdf0baea56ad535be551a
Scylidose <jordisaleilles@hotmail.fr>
2018-05-15 17:39:52 -0400

    Script: Added fadeIn and Ajax.

--- FreeCodeCamp/Wikipedia Viewer/js/script.js
+++ FreeCodeCamp/Wikipedia Viewer/js/script.js
@@ -1,17 +1,27 @@
 $(document).ready(function () {
     $("#search").fadeIn(3000);
+    $("#random").fadeIn(8000);
 });
 
-function rechercher() {
-    if(!$(".words").val()){
-        alert("test");
-
-        $.getJSON('https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5', function(data){
+$(document).ready(function () {
+    $("#search").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'https://en.wikipedia.org/w/api.php',
+            data: { action: 'opensearch', search: $("#words").val(), format: 'json' },
+            dataType: 'jsonp',
+            success: show
         });
-    } else {
-        alert("test2");
+    });
+});
 
-        $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&search=france&limit=1&format=json', function (data) {
-        });
+function show(data) {
+    $(".result").remove();
+
+    for (let i = 0; i < data.length; i++) {
+        setTimeout(function timer() {
+            $("#results").append("<div class='result'><a target='_blank' href='" + data[3][i] + "'><h4><i><b>" + data[1][i] + "</b></i></h4><p>" + data[2][i] + "</p></a></div>");
+            $(".result").fadeIn(3000);
+        }, i * 500);
     }
 }
