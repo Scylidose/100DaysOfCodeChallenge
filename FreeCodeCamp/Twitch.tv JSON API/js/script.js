90550f3ab7971beb0934fac2d742b1e8e4d4c394
Scylidose <jordisaleilles@hotmail.fr>
2018-05-17 05:09:20 -0400

    Script: Added the ability to search if someone is living and added new fadin animation.

--- FreeCodeCamp/Twitch.tv JSON API/js/script.js
+++ FreeCodeCamp/Twitch.tv JSON API/js/script.js
@@ -7,16 +7,28 @@ $(document).ready(function () {
 
         $.getJSON("https://wind-bow.glitch.me/twitch-api/streams/" + utilisateur, function (data) {
             if (data.stream != null) {
-                $("#live").append("<div class='user img-fluid' style='background-image:url(" + data.stream.preview.medium + ")'><a href=" + data.stream.channel.url + "><h5>" + data.stream.channel.display_name + "</h5><p>" + data.stream.channel.status + "</p></a></div>");
-
+                $("#live").append("<div class='user img-fluid' style='background-image:url(" + data.stream.preview.medium + ")'><a target='_blank' href=" + data.stream.channel.url + "><h5>" + data.stream.channel.display_name + "</h5><p>" + data.stream.channel.status + "</p></a></div>");
             } else {
-                $("#live").append("<div class='user img-fluid' style='background-image:url(img/twitch-offline.jpg)'><a href=''><h5></h5><p></p></a></div>");
+                var lastword = data._links.self.split("/").pop();
+                $("#live").append("<div class='user img-fluid' style='background-image:url(img/twitch-offline.jpg)'><a target='_blank' href='https://www.twitch.tv/" + lastword + "'><h5>" + lastword + "</h5></a></div>");
             }
         });
     }
     $("#search").fadeIn(2000);
     $("#live").fadeIn(5000);
 
-});
+    $("#search").submit(function (e) {
+        e.preventDefault();
+        $.getJSON("https://wind-bow.glitch.me/twitch-api/streams/" + $("#words").val(), function (data) {
+            $(".user").remove();
+            if (data.stream != null) {
+                $("#live").append("<div class='user img-fluid' style='background-image:url(" + data.stream.preview.medium + ")'><a target='_blank' href=" + data.stream.channel.url + "><h5>" + data.stream.channel.display_name + "</h5><p>" + data.stream.channel.status + "</p></a></div>").hide().fadeIn(1000);
+            } else {
+                var lastword = data._links.self.split("/").pop();
+                $("#live").append("<div class='user img-fluid' style='background-image:url(img/twitch-offline.jpg)'><a target='_blank' href='https://www.twitch.tv/" + lastword + "'><h5>" + lastword + "</h5></a></div>").hide().fadeIn(1000);
+            }
+        });
+    });
 
+});
 
