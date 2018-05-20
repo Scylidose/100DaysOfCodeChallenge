400c5a768d3cbfbcb83ed00e8b1966c745d4b8d3
Scylidose <jordisaleilles@hotmail.fr>
2018-05-20 17:41:23 -0400

    Script: Added score and attempts mechanics and corrected bug occured with square border.

--- The Web Developer Bootcamp/Color Game/script.js
+++ The Web Developer Bootcamp/Color Game/script.js
@@ -7,51 +7,76 @@ var messageDisplay = document.querySelector("#message");
 var header = document.querySelector(".header");
 var resetButton = document.querySelector("#reset");
 
-resetButton.addEventListener("click", function() {
-	//generate all new colors
+var score = 0;
+var attempts = 5;
+var reseted = true;
+
+resetButton.addEventListener("click", function () {
 	colors = generateRandomColors(6);
-	//pick a new random color from array
 	pickedColor = pickColor();
-	//change colorDisplay to match picked Color
 	colorDisplay.textContent = pickedColor;
-	//change colors of squares
-	for(var i = 0; i < squares.length; i++) {
+
+	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.border = "2px white solid";
+		squares[i].style.opacity = 500;
 	}
 	header.style.backgroundColor = "#232323";
+	reseted = true;
+	if (attempts == 0) {
+		attempts = 5;
+		$("#lives").text(attempts);
+		score = 0;
+		$("#number").text(score);
+	}
 });
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++){
-	// add initial colors to squares
+for (var i = 0; i < squares.length; i++) {
+
 	squares[i].style.backgroundColor = colors[i];
 
-	//add click listeners to squares
-	squares[i].addEventListener("click", function() {
-		//grab color of clicked squares
+	squares[i].addEventListener("click", function () {
 		var clickedColor = this.style.backgroundColor;
-		//compare color to pickedColor
-		if(clickedColor === pickedColor) {
+
+		if (clickedColor === pickedColor) {
+			if (reseted) {
+				score++;
+				$("#number").text(score);
+			}
 			messageDisplay.textContent = "Correct!";
 			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
 			header.style.backgroundColor = clickedColor;
+			reseted = false;
 		} else {
-            this.style.backgroundColor = "#232323";
-            this.style.border = "none";
+			$(this).fadeTo(500, 0, function () {
+				this.style.backgroundColor = "#232323";
+				this.style.border = "2px #232323 solid";
+			});
 			messageDisplay.textContent = "Try Again";
+			if(attempts > 0){
+				attempts--;
+
+			}
+
+			$("#lives").text(attempts);
+			if (attempts == 0) {
+				messageDisplay.textContent = "Game Over!";
+				changeColors("#232323");
+				for (var i = 0; i < squares.length; i++) {
+					squares[i].style.border = "2px #232323 solid";
+				}
+			}
 		}
 	});
 }
 
 function changeColors(color) {
-	//loop through all squares
-	for(var i = 0; i < squares.length; i++) {
-		//change each color to match given color
-        squares[i].style.backgroundColor = color;
-        squares[i].style.border = "2px white solid";
-
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+		squares[i].style.border = "2px white solid";
 	}
 }
 
@@ -62,7 +87,7 @@ function pickColor() {
 
 function generateRandomColors(num) {
 	var arr = [];
-	for(var i = 0; i < num; i++) {
+	for (var i = 0; i < num; i++) {
 		arr.push(randomColor());
 	}
 	return arr;
