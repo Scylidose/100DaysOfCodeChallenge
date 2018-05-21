906870fdf32cb3f360c1e89c6dcdabd09ec72091
Scylidose <jordisaleilles@hotmail.fr>
2018-05-21 04:12:55 -0400

    Script: Added the ability to change the difficulty and the Display message change when we start a new game.

--- The Web Developer Bootcamp/Color Game/script.js
+++ The Web Developer Bootcamp/Color Game/script.js
@@ -1,4 +1,6 @@
-var colors = generateRandomColors(6);
+var numSquares = 9;
+
+var colors = generateRandomColors(numSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -7,19 +9,85 @@ var messageDisplay = document.querySelector("#message");
 var header = document.querySelector(".header");
 var resetButton = document.querySelector("#reset");
 
+var easyBtn = document.querySelector("#easyBtn");
+var mediumBtn = document.querySelector("#mediumBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
 var score = 0;
 var attempts = 5;
 var reseted = true;
 
-resetButton.addEventListener("click", function () {
-	colors = generateRandomColors(6);
+easyBtn.addEventListener("click", function () {
+	mediumBtn.classList.remove("selected");
+	hardBtn.classList.remove("selected");
+
+	easyBtn.classList.add("selected");
+
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
+
+	colorDisplay.textContent = pickedColor;
+
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+mediumBtn.addEventListener("click", function () {
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.remove("selected");
+
+	mediumBtn.classList.add("selected");
+
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+
+	colorDisplay.textContent = pickedColor;
+
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function () {
+	mediumBtn.classList.remove("selected");
+	easyBtn.classList.remove("selected");
+
+	hardBtn.classList.add("selected");
+
+	numSquares = 9;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+
 	colorDisplay.textContent = pickedColor;
 
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+});
+
+resetButton.addEventListener("click", function () {
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	messageDisplay.textContent = "";
+	this.textContent = "New Colors";
+	
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
 		squares[i].style.border = "2px white solid";
-		squares[i].style.opacity = 500;
 	}
 	header.style.backgroundColor = "#232323";
 	reseted = true;
@@ -51,12 +119,10 @@ for (var i = 0; i < squares.length; i++) {
 			header.style.backgroundColor = clickedColor;
 			reseted = false;
 		} else {
-			$(this).fadeTo(500, 0, function () {
-				this.style.backgroundColor = "#232323";
-				this.style.border = "2px #232323 solid";
-			});
+			this.style.backgroundColor = "#232323";
+			this.style.border = "2px #232323 solid";
 			messageDisplay.textContent = "Try Again";
-			if(attempts > 0){
+			if (attempts > 0) {
 				attempts--;
 
 			}
