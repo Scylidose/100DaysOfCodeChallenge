c7e72f43e353f2a58bab22771663406206260e20
Scylidose <jordisaleilles@hotmail.fr>
2018-05-27 01:51:08 -0400

    Script: Added the ability to use the basics arithmetics operations, equal button and can change negative to positive and positive to negative.

--- FreeCodeCamp/JavaScript Calculator/script.js
+++ FreeCodeCamp/JavaScript Calculator/script.js
@@ -1,7 +1,13 @@
 var on = false;
 var interval;
 
+var enters = "";
+var enters2 = "";
+var operation = "";
+var equal = false;
+
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var operations = ["+", "-", "x", "÷"];
 
 $(document).ready(function () {
     $("#init").click(function () {
@@ -26,20 +32,93 @@ $(document).ready(function () {
 
     $("button").click(function () {
         if (on) {
-            if($("input").val() == "I"){
+            if ($("input").val() == "I") {
                 $("input").val("");
             }
-            if (numbers.includes($(this).text())) {
-                var enters = $("input").val() + $(this).text();
-                $("input").val(enters);
+            if (operation != "") {
+                $(".basic").click(function () {
+                    $("input").val("");
+                });
+
+                if (operations.includes($("input").val())) {
+                    if (equal) {
+                        equal = false;
+                    }
+                    $("input").val("");
+                }
+
+                if (numbers.includes($(this).text())) {
+                    if (equal) {
+                        equal = false;
+                        $("input").val("");
+                    }
+                    enters2 = $("input").val() + $(this).text();
+                    $("input").val(enters2);
+                }
+            } else {
+                if (numbers.includes($(this).text())) {
+                    if (equal) {
+                        equal = false;
+                        $("input").val("");
+                    }
+                    enters = $("input").val() + $(this).text();
+                    $("input").val(enters);
+                }
             }
         }
+
+        $(".basic").click(function () {
+            if (enters2 != "") {
+                if (operation == "x") {
+                    operation = "*";
+                } else if (operation == "÷") {
+                    operation = "/";
+                }
+                enters = eval(enters + " " + operation + " " + enters2);
+                operation = "";
+                enters2 = "";
+            }
+            $("input").val($(this).text());
+            operation = $(this).text();
+        });
     });
 });
 
 function reset() {
     if (on) {
         $("#show").val("I");
+    }
+}
+
+function change() {
+    if (on) {
+        var num = 0;
+        if (enters2 == "") {
+            num = parseInt(enters);
+            num *= -1;
+            enters = num.toString();
+
+        } else {
+            num = parseInt(enters2);
+            num *= -1;
+            enters2 = num.toString();
+        }
+        $("input").val(num);
+    }
+}
+
+function result() {
+    if (on) {
+        if (operation == "x") {
+            operation = "*";
+        } else if (operation == "÷") {
+            operation = "/";
+        }
+        enters = eval(enters + " " + operation + " " + enters2);
+        $("input").val(enters);
+        operation = "";
+        enters2 = "";
+        equal = true;
     }
 }
 
