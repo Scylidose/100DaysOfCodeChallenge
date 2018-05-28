6b069af086d3631ee02be1ce51533fad9923feae
Scylidose <jordisaleilles@hotmail.fr>
2018-05-28 05:01:47 -0400

    Script: Created play and stop function, added class management and added new variable for the setter.

--- FreeCodeCamp/Pomodoro Clock/script.js
+++ FreeCodeCamp/Pomodoro Clock/script.js
@@ -1,71 +1,92 @@
 var minutes = 25;
 var secondes = "00";
 
+var minuteReset = minutes;
+var secondeReset = secondes;
+
 var interval;
 
 $(document).ready(function () {
 
-    $("#timer").val(minutes + " : " + secondes);
-
-    $("#start").click(function () {
-        interval = setInterval(function () {
-            if (secondes > "00" && secondes <= "09") {
-                var sec = parseInt(secondes);
-                sec--;
-                secondes = "0" + sec;
-            }
-            if (minutes > "00" && minutes <= "09" && secondes == "00") {
-                var min = parseInt(minutes);
-                min--;
-                minutes = "0" + min;
-                secondes = 60;
-            }
-
-            if (minutes == "00" && secondes == "00") {
-                //Finished
-                clearInterval(interval);
-            }
-
-            if (minutes > "09" && secondes == 60) {
-                minutes--;
-            }
-
-            if (secondes > "09") {
-                secondes--;
-            }
-
-            if (secondes == 9) {
-                secondes = "09"
-            }
-
-            if (minutes == 9) {
-                minutes = "09"
-            }
-
-            $("#timer").val(minutes + " : " + secondes);
-
-            if (secondes == "00") {
-                secondes = 60;
-            }
-
-        }, 1000);
-    });
-
-    $("#stop").click(function () {
-        clearInterval(interval);
-    });
+    $(".timer").val(minutes + " : " + secondes);
 
     $("#reset").click(function () {
-        minutes = 25;
-        secondes = "00";
-        $("#timer").val(minutes + " : " + secondes);
+        minutes = minuteReset;
+        secondes = secondeReset;
+        $(".timer").val(minutes + " : " + secondes);
         clearInterval(interval);
+
+        $(".lecture").replaceWith("<button class='lecture' id='start' onclick='play()'><i class='fas fa-play'></i></button>");
     });
 
     $("#set").click(function () {
         minutes = $("#setMinutes").val();
+        minuteReset = $("#setMinutes").val();
+
         secondes = $("#setSecondes").val();
-        $("#timer").val(minutes + " : " + secondes);
+        secondeReset = $("#setSecondes").val();
+        $(".timer").val(minutes + " : " + secondes);
         clearInterval(interval);
+        $(".lecture").replaceWith("<button class='lecture' id='start' onclick='play()'><i class='fas fa-play'></i></button>");
+
+        $("#setMinutes").val("");
+        $("#setSecondes").val("");
     });
 });
+
+function play() {
+    $("#start").replaceWith("<button class='lecture' id='stop' onclick='stop()'><i class='fas fa-pause'></i></button>");
+    interval = setInterval(function () {
+
+        if($("#clock").hasClass("borderAnim")){
+            $("#clock").removeClass("borderAnim");
+        } else {
+            $("#clock").addClass("borderAnim");
+        }
+
+        if (secondes > "00" && secondes <= "09") {
+            var sec = parseInt(secondes);
+            sec--;
+            secondes = "0" + sec;
+        }
+        if (minutes > "00" && minutes <= "09" && secondes == "00") {
+            var min = parseInt(minutes);
+            min--;
+            minutes = "0" + min;
+            secondes = 60;
+        }
+
+        if (minutes == "00" && secondes == "00") {
+            //Finished
+            clearInterval(interval);
+        }
+
+        if (minutes > "09" && secondes == 60) {
+            minutes--;
+        }
+
+        if (secondes > "09") {
+            secondes--;
+        }
+
+        if (secondes == 9) {
+            secondes = "09"
+        }
+
+        if (minutes == 9) {
+            minutes = "09"
+        }
+
+        $(".timer").val(minutes + " : " + secondes);
+
+        if (secondes == "00") {
+            secondes = 60;
+        }
+
+    }, 1000);
+}
+
+function stop() {
+    $("#stop").replaceWith("<button class='lecture' id='start' onclick='play()'><i class='fas fa-play'></i></button>");
+    clearInterval(interval);
+}
