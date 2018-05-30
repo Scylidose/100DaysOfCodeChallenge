8b4c1c649c62b238e00802f48771f0e7dce639fe
Scylidose <jordisaleilles@hotmail.fr>
2018-05-30 15:30:58 -0400

    Script: Added restart, end, animation when wining (or losing), fadeIn transition and refactored code.

--- FreeCodeCamp/Tic Tac Toe Game/script.js
+++ FreeCodeCamp/Tic Tac Toe Game/script.js
@@ -1,75 +1,109 @@
 var player = "X";
+var won = false;
+var ordre = [];
+var delay = 100;
+var objects =
+    ["#top-left", "#top-middle", "#top-right",
+        "#middle-left", "#middle-middle", "#middle-right",
+        "#bottom-left", "#bottom-middle", "#bottom-right"];
 
 $(document).ready(function () {
-
-    $("#top-left").click(function () {
-        if($("#top-left").text() == ""){
-            $("#top-left").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#top-middle").click(function () {
-        if($("#top-middle").text() == ""){
-            $("#top-middle").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#top-right").click(function () {
-        if($("#top-right").text() == ""){
-            $("#top-right").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#middle-left").click(function () {
-        if($("#middle-left").text() == ""){
-            $("#middle-left").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#middle-middle").click(function () {
-        if($("#middle-middle").text() == ""){
-            $("#middle-middle").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#middle-right").click(function () {
-        if($("#middle-right").text() == ""){
-            $("#middle-right").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#bottom-left").click(function () {
-        if($("#bottom-left").text() == ""){
-            $("#bottom-left").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#bottom-middle").click(function () {
-        if($("#bottom-middle").text() == ""){
-            $("#bottom-middle").text(player);
-            changePlayer();
-        }
-    });
-
-    $("#bottom-right").click(function () {
-        if($("#bottom-right").text() == ""){
-            $("#bottom-right").text(player);
-            changePlayer();
-        }
-    });
+    changeAllText();
 });
 
-function changePlayer(){
-    if(player == "X"){
+function restart() {
+    won = false;
+    ordre = [];
+    delay = 100;
+    emptyCase();
+    $("#result").text("");
+}
+
+function emptyCase(){
+    for(var i = 0; i < objects.length; i++){
+        $(objects[i]).text("");
+        $(objects[i]).removeClass();
+    }
+}
+
+function changeAllText() {
+    for (var i = 0; i < objects.length; i++) {
+        changeText($(objects[i]));
+    }
+}
+
+function changeText(cas) {
+    cas.click(function () {
+        if (!won) {
+            if (cas.text() == "") {
+                cas.text(player);
+                ordre.push(cas);
+                changePlayer();
+            }
+            checkWin();
+        }
+        if (won) {
+            $("#result").text(cas.text() + " Player Won !");
+            $("#restart").fadeIn(2000, function () {
+                $(this).css("display", "block");
+            });
+        }
+    });
+}
+
+function changePlayer() {
+    if (player == "X") {
         player = "O";
     } else {
         player = "X";
     }
+}
+
+function checkWin() {
+    win($(objects[0]), $(objects[1]), $(objects[2])); // _
+    win($(objects[3]), $(objects[4]), $(objects[5])); // -
+    win($(objects[6]), $(objects[7]), $(objects[8])); // -
+    win($(objects[0]), $(objects[4]), $(objects[8])); // \
+    win($(objects[2]), $(objects[4]), $(objects[6])); // /
+    win($(objects[0]), $(objects[3]), $(objects[6])); // |
+    win($(objects[1]), $(objects[4]), $(objects[7])); // |
+    win($(objects[2]), $(objects[5]), $(objects[8])); // |
+}
+
+function win(case1, case2, case3) {
+    if (!won) {
+        if (case1.text() === case2.text() && case1.text() === case3.text() && case1.text() != "") {
+            won = true;
+
+            for (var i = 0; i < ordre.length; i++) {
+                if (ordre[i].attr('id') === case1.attr('id')) {
+                    $(case1).delay(delay).queue(function () { $(this).addClass("win").delay("slow") });
+                    delay += 300;
+                } else if (ordre[i].attr('id') === case2.attr('id')) {
+                    $(case2).delay(delay).queue(function () { $(this).addClass("win").delay("slow") });
+                    delay += 300;
+
+                } else if (ordre[i].attr('id') === case3.attr('id')) {
+                    $(case3).delay(delay).queue(function () { $(this).addClass("win").delay("slow") });
+                    delay += 300;
+
+                } else if (ordre[i].text() != case1.text()) {
+                    $(ordre[i]).fadeIn("slow", function () {
+                        $(this).addClass("loose");
+                    });
+
+                } else if ($(ordre[i]).text() == case1.text()) {
+                    $(ordre[i]).fadeIn("slow", function () {
+                        $(this).addClass("neutral");
+                    });
+                }
+            }
+        }
+    }
+}
+
+function end() {
+    $("#restart").fadeOut(1000, function () {
+        $(this).css("display", "none");
+    });
 }
