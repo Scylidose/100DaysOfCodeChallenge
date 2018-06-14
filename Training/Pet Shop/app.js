58c699dc8af941d02d24f3c4219700004704b460
Scylidose <jordisaleilles@hotmail.fr>
2018-06-14 12:48:47 -0400

    App: Repaired broken links.

--- Training/Pet Shop/app.js
+++ Training/Pet Shop/app.js
@@ -5,16 +5,36 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
+// Home Page
+
 app.get("/", function(req, res){
-    res.render("accueil.ejs", {utilisateur:"None"});
+    res.render("templates/accueil.ejs", {utilisateur:"None"});
 });
 
-app.get("/animaux/page/:page_id", function(req, res){
-    res.render("animaux.ejs");
-});
+// Login and Register Page
 
 app.get("/login", function(req, res){
-    res.render("user.ejs");
+    res.render("templates/user.ejs");
+});
+
+app.get("/enregistrer", function(req, res){
+    res.render("templates/enregistrer.ejs", {error:""});
+});
+
+// Animals Page 
+
+app.get("/animaux/page/:page_id", function(req, res){
+    res.render("templates/animal.ejs");
+});
+
+app.get("/animaux", function(req, res){
+    res.render("templates/animaux.ejs");
+});
+
+// Adoption Page
+
+app.get("/adoption", function(req, res){
+    res.render("templates/adoption.ejs");
 });
 
 app.listen(port , function(){
