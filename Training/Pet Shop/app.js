9d5d14604ccc552545654122a604230946caa2b8
Scylidose <jordisaleilles@hotmail.fr>
2018-06-17 16:10:18 -0400

    App: Added new verification on posted arguments.

--- Training/Pet Shop/app.js
+++ Training/Pet Shop/app.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 // Home Page
 
 app.get("/", function(req, res){
-    res.render("templates/accueil.ejs", {utilisateur:"None"});
+    res.render("templates/accueil.ejs", {utilisateur:"None", error:""});
 });
 
 // Login and Register Page
@@ -30,6 +30,18 @@ app.post("/register", function(req, res){
     var courriel2 = req.body.courriel2;
     var utilisateur = req.body.username;
 
+    if(utilisateur != "None"){
+        res.render("templates/enregistrer.ejs", {error:"Erreur: Nom d'utilisateur déjà pris"});
+    }
+
+    if(destination != courriel2){
+        res.render("templates/enregistrer.ejs", {error:"Erreur: Les courriels sont différents"});
+    }
+
+    if(destination != "None"){
+        res.render("templates/enregistrer.ejs", {error:"Erreur: Adresse courriel déjà prise"});
+    }
+
     var password = req.body.password;
 
     utilisateur = req.body.username;
@@ -39,7 +51,35 @@ app.post("/register", function(req, res){
     var telephone = req.body.telephone;
     var adresse = req.body.adresse;
 
+    if(nom == "" || prenom == "" || utilisateur == "" || telephone == "" || destination == "" || courriel2 == "" || password == "" || adresse == ""){
+        res.render("templates/enregistrer.ejs", {error:"Erreur: Certains champs sont vides"});
+    }
+    
+    var verif_courriel = true;
+
+    if(destination.indexOf('@') <= -1){
+        verif_courriel = false;
+    }
+
+    if(!verif_courriel){
+        res.render("templates/enregistrer.ejs", {error:"Erreur: Le champ du courriel invalide"});
+    }
+
+    var verif_telephone = false;
+
+    if(telephone.match(/^[0-9]+$/) != null){
+        verif_telephone = true;
+    }
+
+    if(!verif_telephone){
+        res.render("templates/enregistrer.ejs", {error:"Erreur: Le champ du téléphone invalide"});
+    }
+
     res.redirect("/confirmer");
+});
+
+app.get("/confirmer", function(req, res){
+    res.render("templates/confirmer.ejs", {param:"confirm"});
 });
 
 // Animals Page 
