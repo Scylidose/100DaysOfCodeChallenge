c81d8630232c35614a09d451e0e92190da76aa07
Scylidose <jordisaleilles@hotmail.fr>
2018-06-21 13:15:18 -0400

    Server: Added new dependency.

--- Training/Pet Shop/server.js
+++ Training/Pet Shop/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,13 +9,20 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // DB Config
 const db = require("./config/key").mongoURI;
 
 // Connect to MongoDB
 mongoose.connect(db).then(() => console.log("MongoDB Connected")).catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
