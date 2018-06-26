dafc5513e17a2bc08fd36b3bc4095ce94ba1d43f
Scylidose <jordisaleilles@hotmail.fr>
2018-06-26 16:11:06 -0400

    Posts: Added post route and validation.

--- Training/Pet Shop/routes/api/posts.js
+++ Training/Pet Shop/routes/api/posts.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const mongoose = require("mongoose");
+
+const Post = require("../../models/Post");
+
+
+const validatePostInput = require("../../validation/post");
 
 /*
     @route    GET api/posts/test
@@ -10,4 +17,24 @@ router.get("/test", (req, res) => res.json({
     msg: "Posts Works"
 }));
 
-module.exports = router;
+/*
+    @route    POST api/posts
+    @desc     Create post
+    @access   Private
+*/
+router.post("/", passport.authenticate("jwt", { session: false}), (req, res) => {
+    
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if(!usValid){
+        return res.status(400).json(errors);
+    }
+    
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+});
