d7ef20ada6cb968c9aa818b681167a3b2218805d
Scylidose <jordisaleilles@hotmail.fr>
2018-06-26 16:11:20 -0400

    Post: Added text validations.

---
+++ Training/Pet Shop/validation/post.js
@@ -0,0 +1,22 @@
+const isEmpty = require("./isempty");
+
+const Validator = require("validator");
+
+module.exports = function validateLoginInput(data) {
+    let errors = {};
+
+    data.text = !isEmpty(data.text) ? data.text : "";
+
+    if(!Validator.isLength(data.text, {min: 10, max: 300})){
+        errors.text = "Post must be between 10 and 300 characters";
+    }
+
+    if (Validator.isEmpty(data.text)) {
+        errors.text = "Text field is required";
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+};
