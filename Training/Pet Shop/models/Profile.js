52c553bbbd18e0fcba815c4cf5b9eb75e1efb5d5
Scylidose <jordisaleilles@hotmail.fr>
2018-06-27 06:18:26 -0400

    Profile: Added profile model.

---
+++ Training/Pet Shop/models/Profile.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
