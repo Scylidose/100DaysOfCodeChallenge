495f1df8943687b1dc3635677b066c956225e67f
Scylidose <jordisaleilles@hotmail.fr>
2018-06-27 06:18:12 -0400

    Server: Added comments.

--- Training/Pet Shop/server.js
+++ Training/Pet Shop/server.js
@@ -1,34 +1,40 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/key").mongoURI;
+const db = require('./config/key').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db).then(() => console.log("MongoDB Connected")).catch(err => console.log(err));
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
