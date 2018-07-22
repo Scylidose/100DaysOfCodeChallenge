32b5626bcd4de31cc127586f8ed229fc18bbd2cc
Scylidose <jordisaleilles@hotmail.fr>
2018-07-22 08:40:35 -0400

    Exercises: Started Login exerise, added key to link mongoDB, added dependencies, first route and passport config.

---
+++ Training/Exercises/NodeJS/Login/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./account/users');
+
+const app = express();
+
+const port = process.env.port || 5000;
+
+const db = require('./config/keys').mongoURI;
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+app.use(express.static("views"));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    res.render('index.ejs');
+});
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.listen(port, function () {
+    console.log(`Server running on port ${port}`);
+});
---
+++ Training/Exercises/NodeJS/Login/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+    'mongodb://Scylidose:ulysse66@ds161740.mlab.com:61740/petshopdatabase',
+    secretOrKey: 'secretKey'
+}
