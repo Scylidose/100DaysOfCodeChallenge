c349f89d6646e587a8fdddd80bfde05c86cdb079
Scylidose <jordisaleilles@hotmail.fr>
2018-07-28 09:18:50 -0400

    Validation: Added validation script.

---
+++ Training/Exercises/NodeJS/Validation/validation/user.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data) {
+    let errors = {};
+
+    data.name = !isEmpty(data.name) ? data.name : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name field is required';
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
