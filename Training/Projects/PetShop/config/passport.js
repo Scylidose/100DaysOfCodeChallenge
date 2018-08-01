ffd81d7bbb87b7bcbd232c22f58199db2f067b05
Scylidose <jordisaleilles@hotmail.fr>
2018-08-01 17:05:39 -0400

    Config: Changed passport id var name.

--- Training/Projects/PetShop/config/passport.js
+++ Training/Projects/PetShop/config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-var User = require('./../models/User');
+const User = require('../models/User');
 const keys = require('../config/keys');
 
 const opts = {};
@@ -11,11 +11,14 @@ opts.secretOrKey = keys.secretOrKey;
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
+      User.findOne({
+        username: jwt_payload._doc._id
+      })
         .then(user => {
           if (user) {
             return done(null, user);
           }
+
           return done(null, false);
         })
         .catch(err => console.log(err));
