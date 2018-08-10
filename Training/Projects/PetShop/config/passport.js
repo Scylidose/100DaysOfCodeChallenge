8bf0af79fc3998cfe109f26f51ce9d4e6debd70a
Scylidose <jordisaleilles@hotmail.fr>
2018-08-10 03:49:18 -0400

    Paasport: Fetch user by username.

--- Training/Projects/PetShop/config/passport.js
+++ Training/Projects/PetShop/config/passport.js
@@ -1,20 +1,22 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const User = mongoose.model('users');
+const User = require('../models/User');
 const keys = require('../config/keys');
 
-var cookieExtractor = function(req) {
+var cookieExtractor = function (req) {
   var token = null;
   if (req && req.cookies) token = req.cookies['jwt'];
   return token;
 };
-module.exports = function(passport) {  
+module.exports = function (passport) {
   var opts = {};
   opts.jwtFromRequest = cookieExtractor; // check token in cookie
   opts.secretOrKey = keys.secretOrKey;
-  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findById(jwt_payload.id, function(err, user) {
+  passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({
+      username: jwt_payload.id
+    }, function (err, user) {
       if (err) {
         return done(err, false);
       }
