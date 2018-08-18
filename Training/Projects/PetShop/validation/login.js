88f4754e5b0bd3aac36671738314d4e62e466c45
Scylidose <jordisaleilles@hotmail.fr>
2018-08-18 01:52:59 -0400

    Validation: Corrected validation to make it coherent.

--- Training/Projects/PetShop/validation/login.js
+++ Training/Projects/PetShop/validation/login.js
@@ -4,15 +4,15 @@ const isEmpty = require('./is-empty');
 module.exports = function validateLoginInput(data) {
     let errors = {};
 
-    data.email = !isEmpty(data.email) ? data.email : '';
+    data.courriel = !isEmpty(data.courriel) ? data.courriel : '';
     data.password = !isEmpty(data.password) ? data.password : '';
 
-    if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
+    if (!Validator.isEmail(data.courriel)) {
+        errors.courriel = 'Email is invalid';
     }
 
-    if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email field is required';
+    if (Validator.isEmpty(data.courriel)) {
+        errors.courriel = 'Email field is required';
     }
 
     if (Validator.isEmpty(data.password)) {
