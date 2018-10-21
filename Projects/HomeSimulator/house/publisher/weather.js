8b243fef383388b375f8e1fc9bc410e61b86abc9
Scylidose <jordisaleilles@hotmail.fr>
2018-10-21 07:59:07 -0400

    Publisher: Is now connected to local adress with port 7777.

---
+++ Projects/HomeSimulator/house/publisher/weather.js
@@ -0,0 +1,23 @@
+var zmq = require('zmq'),
+    publisher = zmq.socket('pub');
+
+var request = require("request");
+
+publisher.bind("tcp://*:7777");
+
+setInterval(() => {
+    request({
+        url: 'https://fcc-weather-api.glitch.me/api/current?lat=45.509498&lon=-73.569394',
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            publisher.send(["weather", body.weather[0].description]);
+            publisher.send(["temperature", body.main.temp]);
+            publisher.send(["humidity", body.main.humidity]);
+            publisher.send(["wind", body.wind.speed]);
+        } else {
+            console.log("Error something bad occured", error);
+        }
+    })
+}, 2000);
