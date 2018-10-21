b0993b7f2edd412dbba331111b5ab7cdd1f25a81
Scylidose <jordisaleilles@hotmail.fr>
2018-10-21 07:59:56 -0400

    App: Removed zmq link.

--- Projects/HomeSimulator/app.js
+++ Projects/HomeSimulator/app.js
@@ -2,7 +2,3 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-
-var zmq = require('zmq'),
-    publisher = zmq.socket('pub'),
-    subscriber = zmq.socket('sub');
